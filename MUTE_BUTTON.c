bada2ff825aa6e0804d6035df13cd1b24135215a
kangwongyu795 <108566980+kangwongyu795@users.noreply.github.com>
2024-08-05 17:32:54 +0900

    test

--- MUTE_BUTTON.c
+++ MUTE_BUTTON.c
@@ -1,16 +1,13 @@
 
 #include <unistd.h> // sleep 함수 사용을 위해 필요한 헤더 파일
+#include <stdio.h>
 
 void muteButtonHandler() {
     static int is = 0; // 초기 상태는 음소거 해제
     is = !is; // 음소거 상태 토글
     if (is) {
         printf("\nAudio system is now MUTED\n");
-
     } else {
         printf("\nAudio system is now UNMUTED\n");
-
-
     }
-    sleep(3);
 }
