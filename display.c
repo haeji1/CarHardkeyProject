821dbb5b8f6a5d33fe3dd06ae311c210297879f2
KangInyeong <kiyoog02@gmail.com>
2024-08-08 06:31:18 +0900

    refactor: delete commands code in display

--- display.c
+++ display.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "display.h"
-#include "observer.h"
-#include "horn.h"
-#include "sunroof.h"
-#include "drive.h"
-#include "steering.h"
-#include "traction.h"
-#include "menu.h"
-#include "headlights.h"
-#include "turnsignals.h"
-#include "windshield.h"
-#include "hazard.h"
-#include "cruise.h"
-#include "radio.h"
 
 void displayMenu() {
     printf("\nAvailable features:\n");
@@ -48,57 +33,4 @@ void displayMenu() {
     printf("38. PARKING_ASSIST\n");
     printf("39. HILL_DESCENT\n");
     printf("50. EXIT\n");
-}
-
-void handleHornCommands() {
-    handlerEvent(HORN);
-}
-
-void handleHeadlightsCommands() {
-
-    handlerEvent(HEADLIGHTS);
-    //unregisterHandler(HEADLIGHTS);
-
-}
-
-void handleTurnSignalsCommands() {
-
-    handlerEvent(TURN_SIGNALS);
-
-}
-
-void handleWindshieldWipersCommands(){
-    handlerEvent(WINDSHIELD_WIPERS);
-}
-
-void handleHazardLightsCommands(){
-    handlerEvent(HAZARD_LIGHTS);
-}
-
-void handleCruiseControlCommands(){
-    handlerEvent(CRUISE_CONTROL);
-}
-
-void handleRadioVolumeCommands(){
-    handlerEvent(RADIO_VOLUME);
-}
-
-void handleRadioTuningCommands(){
-    handlerEvent(RADIO_TUNING);
-}
-
-void initializeFeatureHandlers() {
-    // Initialize handlers for various features
-    initializeHornHandlers();
-    initializeHeadlightsHandlers();
-    initializeTurnSignalsHandlers();
-    initializeWindshieldWipersHandlers();
-    initializeHazardLightsHandlers();
-    initializeCruiseControlHandlers();
-    initializeRadioHandlers();
-}
-
-void cleanupFeatureHandlers() {
-    // Unregister handlers for various features
-    unregisterHandler(HORN, handleHorn);
 }
